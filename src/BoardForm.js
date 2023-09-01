4dcbe876a728e697ccbd8e35c1a1ddab81c22ad2
rla423rla <rla423rla@gmail.com>
2023-09-01 16:01:13 +0900

    homepage1

---
+++ src/BoardForm.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import axios from "axios";
+import $ from "jquery";
+import {} from "jquery.cookie";
+axios.defaults.withCredentials = true;
+const headers = { withCredentials: true };
+
+class BoardRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          <NavLink
+            to={{ pathname: "/board/detail", query: { _id: this.props._id } }}
+          >
+            {this.props.createdAt.substring(0, 10)}
+          </NavLink>
+        </td>
+        <td>
+          <NavLink
+            to={{ pathname: "/board/detail", query: { _id: this.props._id } }}
+          >
+            {this.props.title}
+          </NavLink>
+        </td>
+      </tr>
+    );
+  }
+}
+
+class BoardForm extends Component {
+  state = {
+    boardList: []
+  };
+
+  componentDidMount() {
+    this.getBoardList();
+  }
+
+  getBoardList = () => {
+    const send_param = {
+      headers,
+      _id: $.cookie("login_id")
+    };
+    axios
+      .post("http://localhost:8080/board/getBoardList", send_param)
+      .then(returnData => {
+        if (returnData.data.list) {
+          console.log(returnData.data.list.length);
+          const boards = returnData.data.list;
+          const boardList = boards.map(item => (
+            <BoardRow
+              key={Date.now() + Math.random() * 500}
+              _id={item._id}
+              createdAt={item.createdAt}
+              title={item.title}
+            ></BoardRow>
+          ));
+          // console.log(boardList);
+          this.setState({
+            boardList: boardList
+          });
+        } else {
+          alert("글 목록 조회 실패");
+          window.location.reload();
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    const divStyle = {
+      margin: 50
+    };
+
+    return (
+      <div>
+        <div style={divStyle}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>날짜</th>
+                <th>글 제목</th>
+              </tr>
+            </thead>
+            <tbody>{this.state.boardList}</tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BoardForm;
---
+++ src/LoginForm.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import $ from "jquery";
+import {} from "jquery.cookie";
+axios.defaults.withCredentials = true;
+const headers = { withCredentials: true };
+
+class LoginForm extends Component {
+
+  join = () => {
+    const send_param = {
+      headers,
+      email: this.joinEmail.value,
+      name: this.joinName.value,
+      password: this.joinPw.value
+    };
+    axios
+      .post("http://localhost:8080/member/join", send_param)
+      //정상수행
+      .then(returnData => {
+        if (returnData.data.message) {
+          alert(returnData.data.message);
+        } else {
+          alert("회원가입 실패");
+        }
+      })
+      //에러
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  login = () => {
+    const send_param = {
+      headers,
+      email: this.loginEmail.value,
+      password: this.loginPw.value
+    };
+    axios
+      .post("http://localhost:8080/member/login", send_param)
+      //정상수행
+      .then(returnData => {
+        if (returnData.data.message) {
+          $.cookie("login_id", returnData.data._id);
+          alert(returnData.data.message);
+          window.location.reload();
+        } else {
+          alert("로그인 실패");
+        }
+      })
+      //에러
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  render() {
+    const formStyle = {
+      margin: 50
+    };
+    const buttonStyle = {
+      marginTop: 5
+    };
+
+    return (
+      <Form style={formStyle}>
+        <Form.Group controlId="joinForm">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            ref={ref => (this.joinEmail = ref)}
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Label>name</Form.Label>
+          <Form.Control
+            type="text"
+            ref={ref => (this.joinName = ref)}
+            placeholder="name"
+          />
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            ref={ref => (this.joinPw = ref)}
+            placeholder="Password"
+          />
+          <Button
+            style={buttonStyle}
+            onClick={this.join}
+            variant="primary"
+            type="button"
+            block
+          >
+            회원가입
+         </Button>
+        </Form.Group>
+
+        <Form.Group controlId="loginForm">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            maxLength="100"
+            ref={ref => (this.loginEmail = ref)}
+            placeholder="Enter email"
+          />
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            ref={ref => (this.loginPw = ref)}
+            placeholder="Password"
+          />
+          <Button
+            style={buttonStyle}
+            onClick={this.login}
+            variant="primary"
+            type="button"
+            block
+          >
+            로그인
+          </Button>
+        </Form.Group>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
